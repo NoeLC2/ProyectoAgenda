0625c981540c888ff730a70abd48a6d853301020
xopxelpp <50480465+xopxelpp@users.noreply.github.com>
2019-05-20 13:51:37 +0200

    Lector Internacional
    Lector, clase Internacional

--- src/readers/InternationalReader.java
+++ src/readers/InternationalReader.java
@@ -1,5 +1,30 @@
 package readers;
 
+import java.io.File;
+import java.util.Scanner;
+
+import fileclasses.International;
+
 public class InternationalReader {
-	//El archivo que vaya a leer depende del c�digo que haya en el archivo config
+	
+	public static International getInternacional(){
+		
+		File file = new File("international.CAT");
+		
+		try(Scanner sc = new Scanner(file)) {
+			String line1 = sc.nextLine();
+			String[] result = line1.split(";");
+			int id = Integer.valueOf(result[0]);
+			String value1 = resutl[1];
+			System.out.println(result);
+			
+			String line2 = sc.nextLine();
+			String[] result2 = line2.split(";");
+			int id = result[0];
+			String value2 = result2[1];
+			String value3 = result3[2];
+		}
+		return null;
+		
+	}
 }
